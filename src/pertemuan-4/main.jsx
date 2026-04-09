cf90b6733000c1fd31297b9f4263b25f31237843
fitriana24si@mahasiswa.pcr.ac.id <fitriana24si@mahasiswa.pcr.ac.id>
2026-04-09 16:05:26 +0700

    pertemuan4-lanjutan

--- src/pertemuan-4/main.jsx
+++ src/pertemuan-4/main.jsx
@@ -2,11 +2,13 @@ import { createRoot } from 'react-dom/client'
 import './tailwind.css'
 import FrameworkList from './FrameworkList.jsx'
 import FrameworkListSearchFilter from './FrameworkListSearchFilter.jsx'
+import ResponsiveText from './ResponsiveDesign.jsx'
 
 createRoot(document.getElementById("root"))
   .render(
     <div>
      {/* <FrameworkList/> */}
-     <FrameworkListSearchFilter/>
+     {/* <FrameworkListSearchFilter/> */}
+     <ResponsiveText/>
     </div>
   )
