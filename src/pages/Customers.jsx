f0ee416a8f0fce978d8bdc8a077563c6fa7201b2
fitriana24si@mahasiswa.pcr.ac.id <fitriana24si@mahasiswa.pcr.ac.id>
2026-04-23 15:53:51 +0700

    pertemuan 6

---
+++ src/pages/Customers.jsx
@@ -0,0 +1,13 @@
+import { FaShoppingCart, FaTruck, FaBan, FaDollarSign } from "react-icons/fa";
+import PageHeader from "../components/PageHeader";
+import { useState } from "react";
+
+export default function Customers() {
+
+    return(
+        <div id="dashboard-container">
+            <PageHeader/>
+            <p>Ini halaman customer</p>
+        </div>
+    );
+}
---
+++ src/pages/Orders.jsx
@@ -0,0 +1,13 @@
+import { FaShoppingCart, FaTruck, FaBan, FaDollarSign } from "react-icons/fa";
+import PageHeader from "../components/PageHeader";
+import { useState } from "react";
+
+export default function Orders() {
+
+    return(
+        <div id="dashboard-container">
+            <PageHeader/>
+            <p>Ini halaman ordeer</p>
+        </div>
+    );
+}
